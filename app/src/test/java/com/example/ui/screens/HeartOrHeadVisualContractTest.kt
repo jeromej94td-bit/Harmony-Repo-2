9d28a58893f0583c2733908c54d19d1523b4b5e4
jeromej94td-bit <jeromej94td@gmail.com>
2026-09-05 13:38:51 +0200

    Test: fix Compose semantics assertion import

--- app/src/test/java/com/example/ui/screens/HeartOrHeadVisualContractTest.kt
+++ app/src/test/java/com/example/ui/screens/HeartOrHeadVisualContractTest.kt
@@ -1,7 +1,6 @@
 package com.example.ui.screens
 
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.test.assertExists
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import com.example.ui.theme.HarmonyTheme
